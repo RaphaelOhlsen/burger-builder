c4671b136f6ee79a78040b7120cde340d5eaccbc
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-08-28 18:41:02 -0300

    Starting Implementation of The Burger Builder Container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Teste</p>
+        <BurguerBuilder />
       </Layout>
     </div>
   );
