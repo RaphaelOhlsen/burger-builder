56caf6c32aa39df980e28bf95ed466b4663d4643
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-02 17:09:53 -0300

    Working o Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,7 +6,9 @@ import NavigationItems from '../NavigationItens/NavigationItems';
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <Logo />
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
     <nav>
       <NavigationItems />
     </nav>
