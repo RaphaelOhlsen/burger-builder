43485501d36d93a900d79a7eb509b77a327463ce
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-02 13:22:43 -0300

    Adding a Custom Button Component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,7 +3,8 @@ import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
+const modal = (props) => {
+  return (
   <Aux>
     <Backdrop show={props.show} clicked={props.modalClosed}/>
     <div 
@@ -11,10 +12,12 @@ const modal = (props) => (
       style={{
         transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
         opacity: props.show ? '1' : '0'
-      }}>
+      }}
+    >
       {props.children}
     </div>
   </Aux>
-);
+  )
+};
 
 export default modal;
