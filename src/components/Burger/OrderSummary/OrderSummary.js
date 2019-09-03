c14acd41b7dd35c4b85af47919a28231e92ea841
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-03 19:29:38 -0300

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 import PropTypes from 'prop-types';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+  componentDidUpdate() {
+    console.log('[OrderSummary] Didupdate');
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       )
-    })
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button 
-        btnType="Danger" 
-        clicked={props.purchaseCancelled}>Cancel</Button>
-      <Button 
-        btnType="Success" 
-        clicked={props.purchaseContinued}>Continued</Button>
-    </Aux>
-  )
+    });
+    return (
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button 
+          btnType="Danger" 
+          clicked={this.props.purchaseCancelled}>Cancel</Button>
+        <Button 
+          btnType="Success" 
+          clicked={this.props.purchaseContinued}>Continued</Button>
+      </Aux>
+    )
+  }
 }
 
-orderSummary.propTypes = {
+OrderSummary.propTypes = {
   ingredients: PropTypes.object.isRequired,
   purchaseCancelled: PropTypes.func.isRequired,
   purchaseContinued: PropTypes.func.isRequired,
@@ -39,4 +46,4 @@ orderSummary.propTypes = {
 }
 
 
-export default orderSummary;
+export default OrderSummary;
