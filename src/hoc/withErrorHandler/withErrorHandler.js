7c921b493da2b96c03cc2b37c278260245d132aa
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-09-05 18:01:23 -0300

    Fixing axios error in withErrorHandler.js File

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -2,9 +2,9 @@ import React, { Component }from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
-import axios from 'axios';
+// import axios from 'axios';
 
-const withErrorHandler = (WrappedComponent) => {
+const withErrorHandler = (WrappedComponent, axios) => {
   return class extends Component {
     state = {
       error: null
